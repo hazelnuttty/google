da1e7e60efebfa7ac1e8492f1f684691527dd11a
Hz4E!_ <mrayhan1924@gmail.com>
2025-08-17 14:06:44 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const search = require('./modules/search');
+const images = require('./modules/images');
+const news = require('./modules/news');
+const scholar = require('./modules/scholar');
+const maps = require('./modules/maps');
+const trends = require('./modules/trends');
+
+module.exports = {
+  search,
+  images,
+  news,
+  scholar,
+  maps,
+  trends
+};
